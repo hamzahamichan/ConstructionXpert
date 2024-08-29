d4f850281aac1eca58875e1392204a387e121f3d
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-29 16:37:56 +0100

    creation de controller de taches service

---
+++ microservices/tacheServices/src/main/java/com/feg/tacheServices/controller/ControllerTache.java
@@ -0,0 +1,35 @@
+package com.feg.tacheServices.controller;
+
+import com.feg.tacheServices.entity.Tache;
+import com.feg.tacheServices.service.ITacheser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/t")
+public class ControllerTache {
+    @Autowired
+    private ITacheser iTacheser;
+
+    @PostMapping(value = "/ajouter/tache", consumes = "application/json")
+    public Tache createTache(@RequestBody Tache tache) {
+        return this.iTacheser.createTache(tache);
+    }
+
+    @GetMapping("/mestaches")
+    public List<Tache> recupérerTaches() {
+        return this.iTacheser.recupererTaches();
+    }
+
+    @PutMapping("/editer/{id}")
+    public Tache editTache(@PathVariable Long id, @RequestBody Tache tache) {
+        return this.iTacheser.editTache(id, tache);
+    }
+
+    @GetMapping("/projet/{id}")
+    public List<Tache> recupérerTachesProjet(@PathVariable Long id) {
+        return this.iTacheser.recupererTachesProjet(id);
+    }
+}
