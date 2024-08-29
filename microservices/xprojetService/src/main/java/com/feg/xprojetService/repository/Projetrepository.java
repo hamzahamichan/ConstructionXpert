38042c2089a741d777926ad795fdcea476be5d44
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-29 16:47:15 +0100

    create repo Projet

---
+++ microservices/xprojetService/src/main/java/com/feg/xprojetService/repository/Projetrepository.java
@@ -0,0 +1,9 @@
+package com.feg.xprojetService.repository;
+
+import com.feg.xprojetService.entity.Projet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface Projetrepository extends JpaRepository<Projet,Long> {
+}
