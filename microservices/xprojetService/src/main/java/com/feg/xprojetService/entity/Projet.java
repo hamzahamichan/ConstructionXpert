16ca5bfde4102694214083701bfb2639c733f0b6
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-29 16:45:32 +0100

    création d'entitie Projet

---
+++ microservices/xprojetService/src/main/java/com/feg/xprojetService/entity/Projet.java
@@ -0,0 +1,20 @@
+package com.feg.xprojetService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor @NoArgsConstructor
+@ToString @Data @Entity @Builder @Getter @Setter
+public class Projet {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projetId;
+    private String name;
+    private Date date_debut;
+    private Date date_fin;
+    private Double budget;
+}
