da478026e44ee505ab864001f3764c1612f96592
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-09-06 16:34:31 +0100

    Configuration de class main gatway

--- microservices/apiGetway/src/main/java/com/feg/apiGetway/ApiGetwayApplication.java
+++ microservices/apiGetway/src/main/java/com/feg/apiGetway/ApiGetwayApplication.java
@@ -2,8 +2,10 @@ package com.feg.apiGetway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGetwayApplication {
 
 	public static void main(String[] args) {
